de96ade0254c3b12d61457f8d69813b9d6d58022
nishant-ctrl <knishant9135@gmail.com>
2025-06-19 21:30:29 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 // require("dotenv").config({path:"./env"});
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
-
-
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
+    path: "./env",
+});
+const port = process.env.PORT || 80000;
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERR: ", error);
+            throw error;
+        });
+        app.listen(port, () => {
+            console.log(`Server is running at port: ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGO db connecton failed : ", err);
+    });
 
 // import express from "express";
 // const app = express();
